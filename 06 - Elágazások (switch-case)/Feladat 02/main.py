6e2adc9d33fc40fc8d2881f2a6b76d984b187b69
b1somada <somogyvari.adi@icloud.com>
2022-12-09 07:52:33 +0100

    2022.12.09

---
+++ 06 - Elágazások (switch-case)/Feladat 02/main.py
@@ -0,0 +1,43 @@
+month = input("Adja meg a hónapot szövegesen!")
+
+match month:
+    
+    case "Január":
+        print("1. hónap")
+    
+    case "Február":
+        print("2. hónap")
+    
+    case "Március":
+        print("3. hónap")
+    
+    case "Április":
+        print("4. hónap")
+    
+    case "Május":
+        print("5. hónap")
+    
+    case "Június":
+        print("6. hónap")
+    
+    case "Július":
+        print("7. hónap")
+    
+    case "Augusztus":
+        print("8. hónap")
+    
+    case "Szeptember":
+        print("9. hónap")
+    
+    case "Október":
+        print("10. hónap")
+    
+    case "November":
+        print("11. hónap")
+    
+    case "December":
+        print("12. hónap")
+    
+    case _:
+        print("Nincs ilyen hónap")
+    
